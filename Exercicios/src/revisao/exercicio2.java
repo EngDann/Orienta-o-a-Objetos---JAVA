fa2e26d72618bad6ba120bcbef5b72327147384a
EngDann <danilodemeloribeiro0@gmail.com>
2024-06-06 12:36:22 -0300

    Segundo exercicio revisão

--- Exercicios/src/revisao/exercicio2.java
+++ Exercicios/src/revisao/exercicio2.java
@@ -1,5 +1,27 @@
 package revisao;
 
 public class exercicio2 {
+    public static void main(String[] args) {
+        String Lojas[] = { "Carioca", "Imperatriz" };
+        String Produtos[] = { "Artigos para construção", "Artigos de papelaria", "Roupas" };
+        final Boolean[][] M = {
+                { true, true, true },
+                { false, true, true }
+        };
+        for (int i = 0; i < Lojas.length; i++) {
+            System.out.print(Lojas[i] + ": ");
+            boolean primeiroProduto = true;
+            for (int j = 0; j < Produtos.length; j++) {
+                if (M[i][j]) {
+                    if (!primeiroProduto) {
+                        System.out.print(", ");
+                    }
+                    System.out.print(Produtos[j]);
+                    primeiroProduto = false;
+                }
+            }
+            System.out.println();
+        }
+    }
 
 }
