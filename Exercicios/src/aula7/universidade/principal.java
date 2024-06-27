2a930e659bb7421649827cfef123d87e40c39391
EngDann <danilodemeloribeiro0@gmail.com>
2024-06-27 17:16:08 -0300

    Inicio de principal

--- Exercicios/src/aula7/universidade/principal.java
+++ Exercicios/src/aula7/universidade/principal.java
@@ -1,5 +1,11 @@
 package aula7.universidade;
 
 public class principal {
+    public static void main(String[] args) {
+        Universidade UNB, UFM, UFG = new Universidade();
+        Curso Software, Aeroespacial, Automotiva = new Curso();
+        Disciplina IHC, APC, OO, EA, MCSOL, PEAE = new Disciplina();
+        Aluno aluno1, aluno2, aluno3 = new Aluno();
+    }
 
 }
